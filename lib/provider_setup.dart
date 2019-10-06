47dd6c1726b0fdd604b0ef4b5af350b2b223c1da
KJ <fantasticbabii@gmail.com>
2019-10-06 18:58:06 +0800

    add provider setip

--- lib/provider_setup.dart
+++ lib/provider_setup.dart
@@ -1,1 +1,10 @@
-List<SingleChildClonableWidgets
+import 'package:provider/provider.dart';
+
+List<SingleChildCloneableWidget> providers = [
+  ...independentServices,
+  ...dependentServices,
+  ...uiConsumableProviders
+];
+List<SingleChildCloneableWidget> independentServices = [];
+List<SingleChildCloneableWidget> dependentServices = [];
+List<SingleChildCloneableWidget> uiConsumableProviders = [];
